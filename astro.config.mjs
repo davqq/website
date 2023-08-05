5043270fee81329ea9e83e664d958a62a76bde6c
davqq <davidvongarrel10219@gmail.com>
2023-08-05 20:07:11 +0200

    implement first design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://davqq.de/",
+  integrations: [tailwind()],
+  output: "static",
+  compressHTML: true,
+  trailingSlash: "never",
+  experimental: {
+    assets: true,
+  },
+  redirects: {
+    "/github": {
+      status: 302,
+      destination: "https://github.com/davqq",
+    },
+    "/mail": {
+      status: 302,
+      destination: "mailto:contact@davqq.de",
+    },
+    phonenumber: {
+      status: 302,
+      destination: "tel:+4915735988888",
+    },
+  },
+});
