3ebb3524bf1b7ac2469ee4e779d432d076bce34f
davqq <davidvongarrel10219@gmail.com>
2023-08-25 00:20:34 +0200

    add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roboto-light": ["Roboto-Light"],
+        "roboto-regular": ["Roboto-Regular"],
+        "roboto-medium": ["Roboto-Medium"],
+        "roboto-bold": ["Roboto-Bold"],
+        "roboto-black": ["Roboto-Black"],
+        "outfit-light": ["Outfit-Light"],
+        "outfit-regular": ["Outfit-Regular"],
+        "outfit-medium": ["Outfit-Medium"],
+        "outfit-bold": ["Outfit-Bold"],
+        "outfit-black": ["Outfit-Black"],
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
