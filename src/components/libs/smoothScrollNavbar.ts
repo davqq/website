92952f33132ddb26ca6b0a0f7128ec106b9f3310
davqq <davidvongarrel10219@gmail.com>
2023-09-16 21:19:26 +0200

    better ui/ux

--- src/components/libs/smoothScrollNavbar.ts
+++ src/components/libs/smoothScrollNavbar.ts
@@ -1,17 +1,19 @@
 // Smooth Scrolling für die Navbar-Links
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener("click", (e) => {
     e.preventDefault();
 
-    if (this instanceof HTMLAnchorElement && this.hasAttribute("href")) {
-      const targetId = this.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetId);
+    if (
+      anchor instanceof HTMLAnchorElement &&
+      anchor !== null &&
+      anchor.hasAttribute("href")
+    ) {
+      const targetId = anchor.getAttribute("href")?.substring(1);
+      const targetElement = document.getElementById(targetId || "");
       const header = document.getElementById("header");
 
       if (anchor instanceof HTMLAnchorElement) {
         // change text-decoration with offset and set underline style from font and remove old underline
-        anchor.style.textDecoration = "underline";
-        anchor.style.textUnderlineOffset = "0.2em";
       }
       if (targetElement !== null && header instanceof HTMLElement) {
         window.scrollTo({
