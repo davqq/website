07957774b997a337176027089ff2230b9ac59632
davqq <davidvongarrel10219@gmail.com>
2023-09-17 23:38:07 +0200

    add multilang

---
+++ src/libs/ui.ts
@@ -0,0 +1,29 @@
+export const languages = {
+  en: "English",
+  de: "Deutsch",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.projects": "Projects",
+    "about.title": "About",
+    "about.content":
+      "I'm a 19 year old developer from Germany. I'm currently do an apprenticeship as an IT specialist for application development at <a " +
+      "class='underline decoration-gray-400 decoration-from-font underline-offset-4 duration-500 hover:decoration-black' " +
+      "href='https://www.amcon.de'>Amcon</a>.",
+  },
+  de: {
+    "nav.home": "Start",
+    "nav.about": "Über",
+    "nav.projects": "Projekte",
+    "about.title": "Über mich",
+    "about.content":
+      "Ich bin ein 19 Jahre alter Entwickler aus Deutschland. Ich mache zurzeit eine Ausbildung zum Fachinformatiker für Anwendungsentwicklung bei <a " +
+      "class='underline decoration-gray-400 decoration-from-font underline-offset-4 duration-500 hover:decoration-black' " +
+      "href='https://www.amcon.de'>Amcon</a>.",
+  },
+} as const;
