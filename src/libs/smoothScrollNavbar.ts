1d1c84f218a5a1698f9ab6f46f185283bcfc0f9a
David von Garrel <david.vongarrel@amcon.de>
2023-09-18 21:14:15 +0200

    fix script

--- src/libs/smoothScrollNavbar.ts
+++ src/libs/smoothScrollNavbar.ts
@@ -1,23 +1,27 @@
 // Smooth Scrolling für die Navbar-Links
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", (e) => {
-    e.preventDefault();
 
-    if (
-      anchor instanceof HTMLAnchorElement &&
-      anchor !== null &&
-      anchor.hasAttribute("href")
-    ) {
-      const targetId = anchor.getAttribute("href")?.substring(1);
-      const targetElement = document.getElementById(targetId || "");
-      const header = document.getElementById("header");
+function scroll() {
+  return document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
 
-      if (targetElement !== null && header instanceof HTMLElement) {
-        window.scrollTo({
-          top: targetElement.offsetTop - header.offsetHeight,
-          behavior: "smooth",
-        });
+      if (anchor instanceof HTMLAnchorElement &&
+        anchor !== null &&
+        anchor.hasAttribute("href")) {
+        const targetId = anchor.getAttribute("href")?.substring(1);
+        const targetElement = document.getElementById(targetId || "");
+        const header = document.getElementById("header");
+
+        if (targetElement !== null && header instanceof HTMLElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop - header.offsetHeight,
+            behavior: "smooth",
+          });
+        }
       }
-    }
+    });
   });
-});
+}
+
+scroll();
+document.addEventListener('astro:after-swap', () => scroll());
